56634b72e3cebf4da6901c5d404e1bd997de224c
Takayo Sumi <tky1109sm@gmail.com>
2021-11-12 16:57:19 +0900

    キャラ登録

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function(){
+
+$('#q8-btn').on('click', function () {
+  $(this).siblings().slideToggle(100);
+  // $('#q8-btn ~ dd').slideToggle();
+})
+
+$('#q9-btn').on('click', function () {
+  $(this).siblings().slideToggle(100);
+  // $('#q8-btn ~ dd').slideToggle();
+})
+
+$('#q10-btn').on('click', function () {
+  $(this).siblings().slideToggle(100);
+  // $('#q8-btn ~ dd').slideToggle();
+})
+
+
+
+})

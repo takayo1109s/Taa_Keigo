f5aeab51602008d8ff21b3654386d3e80eccf84b
Takayo Sumi <tky1109sm@gmail.com>
2021-11-14 00:34:05 +0900

    コンテンツの編集

--- script.js
+++ script.js
@@ -16,5 +16,33 @@ $('#q10-btn').on('click', function () {
 })
 
 
+$('.characters button').click(function(){
+  $('.jqs-3 li.blue').show('slow');
+});
+
+
+$(function(){
+  $('.accordion_one .ac_header').click(function(){
+    $(this).next('.ac_inner').slideToggle();
+    $(this).toggleClass("open");
+  });
+});
+
+
+
+document.getElementById("uranai_btn").onclick = function() {
+  var rnd = Math.floor( Math.random() * 6 );
+  var uranai;
+  if (rnd === 0) uranai = "STAR WARS";
+  if (rnd === 1) uranai = "BABY DRIVER";
+  if (rnd === 2) uranai = "ショーシャンクスの空に";
+  if (rnd === 3) uranai = "How To Be Single";
+  if (rnd === 4) uranai = "We Are The Millers";
+  if (rnd === 5) uranai = "LoveHord";
+  alert(uranai);
+}
+
+
+
 
 })
